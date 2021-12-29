88b33bb1c968e433e43fcbfc7b1c5aba5a5e63e2
netwarp <resethread@gmail.com>
2021-12-29 15:52:40 +0100

    add tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,7 @@
-let mix = require('laravel-mix');
+let mix = require('laravel-mix')
+
+require('mix-tailwindcss');
 
 mix.js('resources/js/app.js', 'public/js/app.js').disableNotifications()
+
+mix.postCss('resources/css/app.css', 'public/css/app.css').tailwind().disableNotifications()
