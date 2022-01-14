cd2b7d19774692315b800acb42eccfc2b1266a73
netwarp <resethread@gmail.com>
2022-01-14 15:49:08 +0100

    add redis + controllers

--- routes/routes.mjs
+++ routes/routes.mjs
@@ -1,8 +1,18 @@
 import {Router} from "express";
-
+import AppController from '../controllers/AppController.mjs'
 const router = Router()
 
-router.get('/', (request, response) => response.render('index.html'))
+
+router.get('/', (request, response) => {
+    const data = {
+        wallet: request.session.wallet
+    }
+    response.render('index.html', data)
+})
+
+router.get('/app', AppController.index)
+
 router.get('/rules', (request, response) => response.render('rules.html'))
+
 
 export default router
