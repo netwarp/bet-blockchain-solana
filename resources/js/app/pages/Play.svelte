f90db6a4d06a22952366004a7102d5fd39526284
netwarp <resethread@gmail.com>
2022-02-05 18:54:32 +0100

    prototype works

--- resources/js/app/pages/Play.svelte
+++ resources/js/app/pages/Play.svelte
@@ -2,6 +2,8 @@
     import config from '../_store'
     import axios from 'axios'
 
+    let plays = []
+
     import { io } from "socket.io-client";
     const socket = io();
 
@@ -31,11 +33,57 @@
             address: window.solana.publicKey.toString(),
             signature,
         })
+
+        plays = [{
+            status: 'loading',
+            signature
+        }, ...plays]
+
+        console.log('current plays')
+        console.log(plays)
     }
 
-    socket.on('response', (data) => console.log(data))
+    socket.on('response', (data) => {
+        const play = plays.find(element => element.signature === data.signature)
+        const status = data.status
+
+        play.status = status
+
+        plays = plays
+
+        console.log(data)
+        console.log(play)
+        console.log(status)
+    })
 </script>
 
 <div id="view-play">
-    <button type="button" on:click={play}>Bet</button>
+
+    <button type="button" on:click={play}>Play</button>
+
+    <div class="box-result">
+        <div class="box-result-header">
+            Results:
+        </div>
+        <div class="box-result-lines">
+            {#each plays as play}
+                <div class="box-result-line">
+                    <div class="box-result-lite-status">
+                        {#if play.status === 'loading'}
+                            <img src="/images/icon-load.svg" alt="" class="loader">
+                        {:else if play.status === 'won' }
+                            <img src="/images/icon-won.svg" alt="">
+                        {:else if play.status === 'lost' }
+                            <img src="/images/icon-lost.svg" alt="">
+                        {/if}
+                    </div>
+                    <div class="box-result-lite-signature">
+                        <a href="https://explorer.solana.com/tx/{play.signature}?cluster=devnet" target="_blank">
+                            {play.signature}
+                        </a>
+                    </div>
+                </div>
+            {/each}
+        </div>
+    </div>
 </div>
