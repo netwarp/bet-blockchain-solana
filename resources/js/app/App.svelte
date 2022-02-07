9db26daf5d2e3b2791d1025fdd813b5a38b850f5
netwarp <resethread@gmail.com>
2022-02-07 11:12:06 +0100

    update svelte store connect

--- resources/js/app/App.svelte
+++ resources/js/app/App.svelte
@@ -1,17 +1,56 @@
 <script>
-    console.log('ok')
+    import {view, is_connected, public_key} from './_store'
+
+    import Play from './pages/Play.svelte'
+
+   // let is_connected = false
+    //let public_key = ''
+
+    const getProvider = async () => {
+        if ("solana" in window) {
+            const provider = window.solana
+            if (provider.isPhantom) {
+                return provider
+            }
+        }
+        window.open("https://phantom.app/", "_blank")
+    }
+
+    async function connectWallet() {
+        const provider = await getProvider()
+        const response = await provider.connect()
+        const response_public_key = response.publicKey.toString()
+        public_key.set(response_public_key)
+        $is_connected = true
+    }
+
+    function disconnect() {
+        window.solana.disconnect()
+        $is_connected = false
+    }
+
+
 </script>
 
 <div class="container">
     <div class="sidebar">
-        <div class="connect-wallet">Connect wallet </div>
-
-        <div class="sidebar-item">Play</div>
-        <div class="sidebar-item">Rewards</div>
-        <div class="sidebar-item">History</div>
-        <div class="sidebar-item">Logout</div>
+        {#if $is_connected === false}
+            <div class="connect-wallet" on:click={connectWallet}>Connect wallet</div>
+        {:else }
+            <div class="connect-wallet key">{$public_key}</div>
+        {/if}
+        <div class="sidebar-item {$view === 'play' ? 'active' : '' }" on:click={ () => $view = 'play' }>Play</div>
+        <div class="sidebar-item {$view === 'rewards' ? 'active' : '' }" on:click={ () => $view = 'rewards' }>Rewards</div>
+        <div class="sidebar-item {$view === 'history' ? 'active' : '' }" on:click={ () => $view = 'history' }>History</div>
+        <div class="sidebar-item" on:click={disconnect}>Logout</div>
     </div>
     <div class="main">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolore nesciunt nulla odit officia quaerat quibusdam recusandae! Atque esse necessitatibus omnis quia sunt? Eos mollitia odio reprehenderit sint, sunt veniam!
+        {#if $view === 'play'}
+            <Play />
+        {:else if $view === 'rewards'}
+            Rewards
+        {:else if $view === 'history'}
+            History
+        {/if}
     </div>
 </div>
--- resources/js/app/_store.js
+++ resources/js/app/_store.js
@@ -1,5 +1,7 @@
 import {writable} from 'svelte/store'
 export const view = writable('play')
+export const is_connected = writable(false)
+export const public_key = writable(null)
 
 import config from '../../../config.json'
 export default config
