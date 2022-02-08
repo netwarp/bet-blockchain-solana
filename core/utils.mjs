6464fe9430add38d0f9fbb26a7fb1292ae0c8c8f
netwarp <resethread@gmail.com>
2022-02-08 16:49:45 +0100

    create utils files in core

---
+++ core/utils.mjs
@@ -0,0 +1,38 @@
+export function percent(total, percent) {
+    return Math.round(percent * 100 / total)
+}
+
+/**
+ *
+ * @param {string} char
+ * @return {Boolean}
+ */
+export function isNumber(char) {
+    return char.length && isFinite(char)
+}
+
+/**
+ *
+ * @param {string} hash
+ */
+export function getLatestNumberFromHash(hash) {
+    if ( ! hash.length) {
+        return false
+    }
+
+    const length = hash.length
+
+    let latest_number = null
+
+    for (let i = length -1; i > 0; i--) {
+
+        const current_char = hash.at(i)
+
+        if (isNumber(current_char)) {
+            latest_number = current_char
+            break
+        }
+    }
+
+    return parseInt(latest_number)
+}
