b99daed780b24db31d5bec5331fe633bf20fcfb5
netwarp <resethread@gmail.com>
2022-03-27 17:37:37 +0200

    finished project ?

--- controllers/AppController.mjs
+++ controllers/AppController.mjs
@@ -1,4 +1,6 @@
 import Transaction from '../models/Transaction.mjs'
+import Reward from '../models/Reward.mjs'
+import config from '../services/config.mjs'
 
 export async function index(request, response) {
     response.render('index.html')
@@ -14,12 +16,33 @@ export async function rules(request, response) {
 
 export async function stats(request, response) {
     const transactions = await Transaction.count()
+    const network = config.solana.network
+
+
+    const limit = 30
+    const page = request.body.page ?? 1
+
+    const offset = page > 1 ? (page - 1) * limit : 0
+
+    const rewards = await Reward.findAndCountAll({
+        order: [
+            ['id', 'desc']
+        ],
+        offset,
+        limit
+    })
+
+    let pages = rewards.count / limit
+    pages = Math.round(pages)
+
 
     const data = {
-        transactions
+        transactions,
+        rewards,
+        network,
+        page,
+        pages
     }
-
-    console.log(data)
 
     response.render('stats.html', data)
 }
