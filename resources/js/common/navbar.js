44679d55c5a272700089b4e75ff50ed5520e82ec
netwarp <resethread@gmail.com>
2022-03-27 15:19:25 +0200

    up responsive

--- resources/js/common/navbar.js
+++ resources/js/common/navbar.js
@@ -1,3 +1,32 @@
 export default function navbar() {
 
+	const icon_burger = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-justify" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
+    </svg>`
+
+	const icon_cross = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z"/>
+        <path fill-rule="evenodd" d="M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z"/>
+	</svg>`
+
+	const menu_responsive = document.querySelector('.menu-responsive')
+
+	! function fillMenuContent () {
+		const content = document.querySelector('.menu').outerHTML
+		menu_responsive.innerHTML = content
+	}()
+
+	const icon_menu = document.querySelector('.icon-menu')
+
+	function switchIcon(icon) {
+		icon_menu.innerHTML = icon
+	}
+
+	let hidden = true
+
+	icon_menu.addEventListener('click', () => {
+		hidden ? switchIcon(icon_cross) : switchIcon(icon_burger)
+		menu_responsive.classList.toggle('active')
+		hidden = ! hidden
+	})
 }
