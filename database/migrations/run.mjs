19717a7637d4feb5cb6f375191c7e178c5a94211
netwarp <resethread@gmail.com>
2022-04-02 16:36:48 +0200

    Update run.mjs

--- database/migrations/run.mjs
+++ database/migrations/run.mjs
@@ -1,20 +1,24 @@
-// TODO debug
-
 import fs from 'fs'
+import pkg from 'pg';
+const {Pool, Client} = pkg;
+
 import config from '../../services/config.mjs'
 const pool = new Pool({
     user: config.database.username,
     host: 'localhost',
     database: config.database.name,
     password: config.database.password,
-    port: 3211,
+    port: 5432,
 })
-
 
 async function migrate() {
     let sql = fs.readFileSync(new URL('./main.sql', import.meta.url), 'utf8')
+
+    await pool.query(sql)
+    await pool.end()
 }
 
 await migrate()
-//console.log('\x1b[32m', 'Script finished, but check your database anyway' ,'\x1b[0m');
+console.log('\x1b[32m', 'Script finished, but check your database anyway' ,'\x1b[0m')
+
 process.exit()
